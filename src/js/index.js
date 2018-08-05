4310055eb976243b6d25fa35e96baac4ab302322
Blake <bsward8@gmail.com>
2018-08-05 12:26:00 +1000

    Change background image and minor improvements

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,30 @@
 const portfolioCards = {
-	"zBot": "zBot was a multi-purpose Discord bot application that allowed users to use commands to make their experience on their Discord server more enjoyable and interactive. It provided moderation commands to punish people for breaking rules, general purpose commands for advanced and detailed server management, as well as other fun commands to play around with.",
-	"Remindo": "Remindo is a desktop application made in Electron with the goal of being a viable replacement for Google Keep by providing advanced reminder options such as timers, comments and markdown whilst maintaining a sleek and simplistic design."
+	"zBot": {
+		description: "zBot was a multi-purpose Discord bot application that allowed users to use commands to make their experience on their Discord server more enjoyable and interactive. It provided moderation commands to punish people for breaking rules, general purpose commands for advanced and detailed server management, as well as other fun commands to play around with.",
+		github: "zBlakee/zBot",
+		website: "https://zbot.me",
+		avatar: "https://cdn.discordapp.com/avatars/345766303052857344/b65473af3e075ba54d400e276e55dd0b.png?size=2048"
+	},
+	"Remindo": {
+		description: "Remindo is a desktop application made in Electron with the goal of being a viable replacement for Google Keep by providing advanced reminder options such as timers, comments and markdown whilst maintaining a sleek and simplistic design.",
+		github: "zBlakee/remindo"
+	}
 }
 
-const addPortfolioCard = (title, description) => {
+const addPortfolioCard = (title, info) => {
 	let html = "";
 	html += `
 	<div class="col s12 m6">
 		<div class="card blue-grey darken-1 z-depth-1">
 			<div class="card-content white-text">
-				<span class="card-title"><b>${title}</b></span>
-				<p>${description}</p>
+				<span class="card-title">${info.avatar ? `<img class="avatar-small" src="${info.avatar}">` : ""} <b>${title}</b></span>
+				<p>${info.description}</p>
 			</div>
 
 			<div class="card-action">
-				<a href="#" class="btn cyan darken-3">More Information</a>
+				<a href="#" class="btn red lighten-1">More Information</a>
+				${info.github ? `<a href="https://github.com/${info.github}" style="background-color: #333" class="btn">GitHub</a>` : ""}
+				${info.website ? `<a href="${info.website}" class="btn light-green darken-1">Website</a>` : ""}
 			</div>
 		</div>
 	</div>
