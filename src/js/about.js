20d2e146f59c727b2448b8c3093bf7f922e46c8f
Blake <bsward8@gmail.com>
2019-03-28 01:26:12 +1100

    feat: use similar text handling animations to index.js

---
+++ src/js/about.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", async function (event) {
+	const mainTitle = document.getElementsByClassName('main-title')[0];
+
+	const greetingSplit = [ 'But,', 'who', 'am', 'I', 'really?' ].reverse();
+	const splitInnerHTML = mainTitle.innerHTML.split('')
+	for (let word in greetingSplit) {
+		splitInnerHTML.unshift(`<div class="word-main-title" style="visibility: collapse;">${greetingSplit[word]}</div>`)
+		if (word) {
+			splitInnerHTML.unshift(' ')
+		}
+	}
+
+	const displayAbout = () => {
+		const back = document.getElementsByClassName('lower-half')[0];
+		const about = document.getElementsByClassName('about')[0];
+		setTimeout(() => {
+			about.style = back.style = '';
+			about.classList.add('animated', 'animatedFadeInUp', 'fadeInUp');
+			back.classList = about.classList;
+		}, 400);
+	}
+
+	mainTitle.innerHTML = splitInnerHTML.join('');
+	// Represents the times between the next animation (100ms, then wait xms until next)
+	const timings = [ 100, 460, 140, 70, 150 ];
+
+	let index = 0;
+	const queue = [ ...mainTitle.children ];
+	const callNextAnimation = () => {
+		const elm = queue[index]
+		if (!elm) {
+			// We're done with the introduction now, display description
+			displayAbout();
+			return;
+		}
+
+		const timing = timings[index];
+		setTimeout(() => {
+			elm.style = '';
+			elm.classList.add('animated', 'animatedFadeInUp', 'fadeInUp');
+			callNextAnimation()
+		}, timing || 100);
+		index++;
+	}
+	callNextAnimation()
+	// Finally, remove the style from main title
+	mainTitle.style = '';
+});
