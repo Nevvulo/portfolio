c708e580866716063642f4286baa4765ab1cfc56
Blake Ward <26913683+Nevvulo@users.noreply.github.com>
2021-06-07 00:09:57 +1000

    Remove lazy-loading from pages
    until a better solution is found where the pages don't blink into existence this is going

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,10 @@ import { AnimatedRoutes } from "./components/routing/animated-routes";
 import { RouteTransition } from "./components/routing/route-transition";
 import { ROUTES } from "./constants/routes";
 import "./styles.module.scss";
-
-const About = lazy(() => import("./pages/about"));
-const Contact = lazy(() => import("./pages/contact"));
-const Home = lazy(() => import("./pages/home"));
-const Projects = lazy(() => import("./pages/projects"));
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+import Home from "./pages/home";
 
 const Content: React.FC = () => {
   return (
